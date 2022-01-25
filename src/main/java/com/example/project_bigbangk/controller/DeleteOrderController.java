b4ca6f6a74da6490d9a42fb6a7ae7b5f3c4ebe83
Karim <karim.ajour@hva.nl>
2022-01-25 14:25:50 +0100

    DeleteOrderService aangemaakt en de basis gelegd voor de methode in de DeleteOrderController

--- src/main/java/com/example/project_bigbangk/controller/DeleteOrderController.java
+++ src/main/java/com/example/project_bigbangk/controller/DeleteOrderController.java
@@ -1,7 +1,8 @@
 package com.example.project_bigbangk.controller;
 
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.RestController;
+import com.example.project_bigbangk.service.Security.AuthenticateService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
 /**
  * Created by Karim Ajour on 25-1-2022 for Project_Big_Bangk
@@ -11,9 +12,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class DeleteOrderController {
 
+    private AuthenticateService authenticateService;
 
+    public DeleteOrderController(AuthenticateService authenticateService) {
+        this.authenticateService = authenticateService;
+    }
 
-
+    @PostMapping("/deleteorder")
+    @ResponseBody
+    public ResponseEntity deleteOrder(@RequestHeader String authorization, @RequestBody int orderID) {
+        if (authenticateService.authenticate(authorization)) {
+            //stuur orderid naar methode in service (ook service in constructor van controller hierboven^)
+            //in de service moet je op de een of andere manier bij delete order komen, moet dus een delete order methode in orderDAO
+            //en die moet je via de rootrepo aanroepen
+            //en als laatste response entity terugsturen met ok of zoiets
+        }
+        return null;
+    }
 
 
 }
---
+++ src/main/java/com/example/project_bigbangk/service/DeleteOrderService.java
@@ -0,0 +1,7 @@
+package com.example.project_bigbangk.service;
+
+/**
+ * Created by Karim Ajour on 25-1-2022 for Project_Big_Bangk
+ */
+public class DeleteOrderService {
+}
