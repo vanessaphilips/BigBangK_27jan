2841ed17630ebd0d8b780c22db815653f4f7b9c4
Karim <karim.ajour@hva.nl>
2022-01-25 10:36:21 +0100

    DeleteOrderController aangemaakt

---
+++ src/main/java/com/example/project_bigbangk/controller/DeleteOrderController.java
@@ -0,0 +1,19 @@
+package com.example.project_bigbangk.controller;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by Karim Ajour on 25-1-2022 for Project_Big_Bangk
+ */
+
+@CrossOrigin
+@RestController
+public class DeleteOrderController {
+
+
+
+
+
+
+}
