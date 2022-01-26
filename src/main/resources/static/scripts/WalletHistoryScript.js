3e5fc591ced8f6ca9aebc4cdc3fc9adf6f80f83b
Kelly Speelman <kelly.speelman-de.jonge@hva.nl>
2022-01-27 00:08:24 +0100

    frondsite opgezet van WalletHistory(html, css en js). Er wordt nu 1 json string gebruikt om drie grafieken te vullen die verschillende infromatie nodig hebben. Nu moet het alleen nog gaan werken met de backend die ook op de juiste manier de json moet gaan sturen.

--- src/main/resources/static/scripts/WalletHistoryScript.js
+++ src/main/resources/static/scripts/WalletHistoryScript.js
@@ -1,6 +1,54 @@
+/**
+ *
+ * @Author Kelly Speelman - de Jonge
+ */
+
 google.charts.load('current', {'packages':['corechart']});
-google.charts.setOnLoadCallback(getInfromation);
+google.charts.setOnLoadCallback(drawCharts);
 let token = localStorage.getItem(JWT_KEY);
+
+function drawCharts(){
+    /*let token = "hoi@hotmail.nl"; // test token voor het testen van de back met de frond end.
+    getWalletHistorie(token);*/
+    let valutaData = '{"piechart": ['+
+        '{"Valuta":"Euro","Price":20},'+
+        '{"Valuta":"Cardano","Price":200},'+
+        '{"Valuta":"Cardano","Price":70},'+
+        '{"Valuta":"Litecoin","Price":30}' +
+        '],"linechart": ['+
+    '{"dateTime":"2022-01-01T14:53:41","Euro":20, "All crypto":200},'+
+    '{"dateTime":"2022-01-01T14:53:41","Euro":20, "All crypto":210},'+
+    '{"dateTime":"2022-01-01T14:53:41","Euro":100, "All crypto":100}' +
+        '],"barchart": ['+
+    '{"Valuta":"Euro","old":20,"nieuw":20},'+
+    '{"Valuta":"Cardano","old":200,"nieuw":210},'+
+    '{"Valuta":"Litecoin","old":20,"nieuw":30}]}';
+    let obj = JSON.parse(valutaData);
+    //document.getElementById("test").innerHTML = obj["piechart"];
+
+    const dataPie = google.visualization.arrayToDataTable(getInfromationPieChart(obj["piechart"]));
+    const dataLine = google.visualization.arrayToDataTable(getInfromationLineChart(obj["linechart"]));
+    const dataBar = google.visualization.arrayToDataTable(getInfromationBarChart(obj["barchart"]));
+
+    const options = {
+        //title: 'My Wallet',
+        is3D: true,
+        pieSliceTextStyle: {
+            color: 'black',
+        },
+        //colors: ['#b0120a', '#ffab91'],
+        sliceVisibilityThreshold: 0.08,
+        backgroundColor: 'none',
+    };
+
+    const piechart = new google.visualization.PieChart(document.getElementById('piechart'));
+    const linechart = new google.visualization.LineChart(document.getElementById('linechart'));
+    const barchart = new google.visualization.BarChart(document.getElementById('barchart'));
+
+    piechart.draw(dataPie, options);
+    linechart.draw(dataLine, options);
+    barchart.draw(dataBar, options);
+}
 
 const getWalletHistorie = async (token) => {
     return await fetch(`${rootURL}walletHistory`,
@@ -21,49 +69,27 @@ const getWalletHistorie = async (token) => {
         .catch(error=>console.log("Somethin went wrong: " + error))
 }
 
-function getInfromation() {
+function getInfromationPieChart(obj) {
     let dataArray = [['Valuta', 'Price']];
-
-    let valutaData = '['+
-        '{"Euro":20},'+
-        '{"Cardano":200},'+
-        '{"Litecoin":30}]';
-
-    let obj = JSON.parse(valutaData);
-    //document.getElementById('tekst').innerHTML = obj.Wallet;
-    Object.entries(obj).forEach((entry) => {
-        const [key, value] = entry;
-        Object.entries(value).forEach((entry) =>{
-            const [key1, value1] = entry;
-            dataArray.push([key1, value1]);
-        });
-    });
-
-    drawCharts(dataArray);
+    for (const x in obj) {
+        dataArray.push([obj[x]["Valuta"], obj[x]["Price"]]);
+    }
+    return dataArray;
 }
 
-function drawCharts(dataArray){
-
-    const data = google.visualization.arrayToDataTable(dataArray);
-
-    const options = {
-        title: 'My Wallet',
-        is3D: true,
-        pieSliceTextStyle: {
-            color: 'black',
-        },
-        //sliceVisibilityThreshold: 0.08,
-        backgroundColor: 'none',
-        //colors: ['Yellow', 'orange', 'red', 'purple', 'green']
-    };
-
-    const piechart = new google.visualization.PieChart(document.getElementById('piechart'));
-    const linechart = new google.visualization.LineChart(document.getElementById('linechart'));
-    const barchart = new google.visualization.BarChart(document.getElementById('barchart'));
-
-    piechart.draw(data, options);
-    linechart.draw(data, options);
-    barchart.draw(data, options);
+function getInfromationLineChart(obj) {
+    let dataArray = [['Date', 'Euro', 'All Crypto', 'Total']];
+    for (const x in obj) {
+        let totaal = parseInt(obj[x]["Euro"]) + parseInt(obj[x]["All crypto"]);
+        dataArray.push([obj[x]["dateTime"], obj[x]["Euro"], obj[x]["All crypto"], totaal]);
+    }
+    return dataArray;
 }
 
-getWalletHistorie(token);
+function getInfromationBarChart(myobj) {
+    let dataArray = [['Valuta', 'Purchase price', 'Current price']];
+    for (const x in myobj) {
+        dataArray.push([myobj[x]["Valuta"], myobj[x]["old"], myobj[x]["nieuw"]])
+    }
+    return dataArray;
+}
